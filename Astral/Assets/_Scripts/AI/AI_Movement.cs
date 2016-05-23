ee3abc106ab256f45d6e576a9a29e6133859e8e3
waterkh <pjclark@email.sc.edu>
2016-05-23 08:24:48 -0400

    First Commit + AI Movement
    Created a couple folders (Scripts and Scenes) and one script
    (AI_Movement.cs)
    
    + AI_Movement - Controls the movement of the enemy character (Simple
    enemies - Once I get more ideas I can implement into a separate script,
    or simply create a big API for enemy movement where they can call from
    it.)
    
    + Bugs (or things to fix) - The turning around is a little too quick.
    We can maybe create an animation to play where they turn around, or I
    can program a type of timer that slows down their movement while they
    turn around.

---
+++ Astral/Assets/_Scripts/AI/AI_Movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_Movement : MonoBehaviour {
+
+	// Variables For Movement
+	private Vector3 movementSpeed = new Vector2(5.0f, 0.0f);
+	private float leftLimit = 0.0f;
+	private float rightLimit = 0.0f;
+	private bool firstPass = false; 
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// This sets up the limits - We do this in a first pass just in
+		// case they follow a player. That way they come back to where they started at.
+		// !! Subject to change !!
+		if(!firstPass)
+		{
+			leftLimit = transform.position.x - 10.0f;
+			rightLimit = transform.position.x + 10.0f;
+			firstPass = true;
+		}	
+
+		// Controls AI movement
+		transform.position += movementSpeed * Time.deltaTime;
+
+		// Changes direction when we reach limits
+		if(transform.position.x >= rightLimit || transform.position.x <= leftLimit)
+		{
+			movementSpeed = -movementSpeed;
+		}
+	}
+}
