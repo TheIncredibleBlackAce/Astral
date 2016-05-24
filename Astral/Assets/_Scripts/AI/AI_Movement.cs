cf2e75fab55e10e98360df037e03b713eb524d73
waterkh <pjclark@email.sc.edu>
2016-05-24 10:15:29 -0400

    Added Class + Folders + Objects In Scene
    + Enemy.cs Added
    
    + AI_Movement - Added Player Detection. Not finished, but working. Next
    step is to add better detection by making two rays and then findings
    10-15 rays between these so as to catch player crouch and player jump.
    
    + Added BaseClasses Folder - This will be used for classes like
    Enemy.cs and Player.cs; Possible Boss.cs. Classes that need stats that
    can be changed without making entirely new classes.
    
    + Added Player sprite to scene with a BoxCollider2D.

--- Astral/Assets/_Scripts/AI/AI_Movement.cs
+++ Astral/Assets/_Scripts/AI/AI_Movement.cs
@@ -8,10 +8,19 @@ public class AI_Movement : MonoBehaviour {
 	private float leftLimit = 0.0f;
 	private float rightLimit = 0.0f;
 	private bool firstPass = false; 
-	
+
+	// Variables For Player Detection 
+	public bool playerDetected = false;
+	private Vector3 direction = Vector3.zero;
+	private float previousTransform = 0.0f;
+	private Vector3 rayToCast = Vector3.zero;
+	private float distance = 10.0f;
+
 	// Update is called once per frame
 	void Update () 
 	{
+		/*	START OF MOVEMENT */
+
 		// This sets up the limits - We do this in a first pass just in
 		// case they follow a player. That way they come back to where they started at.
 		// !! Subject to change !!
@@ -30,5 +39,39 @@ public class AI_Movement : MonoBehaviour {
 		{
 			movementSpeed = -movementSpeed;
 		}
+
+		/*	END OF MOVEMENT */
+
+		/* 	START OF DETECTION */
+
+		// Use Speed Calculated Before To Find Direction
+		if(movementSpeed.x > 0)
+		{
+			direction = Vector3.right;
+			print("Right");
+		}
+		else
+		{	
+			direction = Vector3.left;
+			print("Left");
+		}
+
+		previousTransform = transform.position.x;
+
+		// RayCast To Hit Our Player
+		//TODO Change this so that we perform two raycasts (eg. one diagonal down and one diagonal up)
+		// and then raycast several rays between them.
+		rayToCast = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+
+		if(Physics2D.Raycast(rayToCast, direction, distance))
+		{
+			print("Hit something");
+			playerDetected = true;
+		}
+		else
+		{
+			playerDetected = false;
+		}
+
 	}
 }
---
+++ Astral/Assets/_Scripts/BaseClasses/Enemy.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy {
+
+	private float health;
+	private float movementSpeed;
+	private float attackSpeed;
+	private float attackDamage;
+	private float defense;
+
+
+	/*	Constructors	*/
+
+	public Enemy()
+	{
+		this.setHealth(100.0f);
+		this.setMovementSpeed(2.0f);
+		this.setAttackSpeed(1.0f);
+		this.setDamage(5.0f);
+		this.setDefense(2.5f);
+	}
+
+	public Enemy(float aHealth, float moveSpeed, float attSpeed, float attDamage, float aDef)
+	{
+		this.setHealth(aHealth);
+		this.setMovementSpeed(moveSpeed);
+		this.setAttackSpeed(attSpeed);
+		this.setDamage(attDamage);
+		this.setDefense(aDef);
+	}
+
+	// Don't Know If This Will Be Used - Length of 5 For Array
+	public Enemy(float[] stats)
+	{
+		this.setHealth(stats[0]);
+		this.setMovementSpeed(stats[1]);
+		this.setAttackSpeed(stats[2]);
+		this.setDamage(stats[3]);
+		this.setDefense(stats[4]);
+	}
+
+	/*	Mutators	*/
+
+	public void setHealth(float aHealth)
+	{
+		health = aHealth;
+	}
+	public void setMovementSpeed(float moveSpeed)
+	{
+		movementSpeed = moveSpeed;
+	}
+	public void setAttackSpeed(float attSpeed)
+	{
+		attackSpeed = attSpeed;
+	}
+	public void setDamage(float attDamage)
+	{
+		attackDamage = attDamage;
+	}
+	public void setDefense(float aDef)
+	{
+		defense = aDef;
+	}
+
+	/*	Accessors	*/
+
+	public float getHealth()
+	{
+		return health;
+	}
+	public float getMovementSpeed()
+	{
+		return movementSpeed;
+	}
+	public float getAttackSpeed()
+	{
+		return attackSpeed;
+	}
+	public float getDamage()
+	{
+		return attackDamage;
+	}
+	public float getDefense()
+	{
+		return defense;
+	}
+}
