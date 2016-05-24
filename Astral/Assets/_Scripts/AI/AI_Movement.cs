bb2b68edf35df8a1dc2fd1ad7378f8a7a3779738
waterkh <pjclark@email.sc.edu>
2016-05-24 11:31:12 -0400

    More Simple Player Detection
    + Added Trigger detection instead of RayCasting - Much less work.
    
    + Added a flip character to AI_Movement instead of just reversing
    movement.

--- Astral/Assets/_Scripts/AI/AI_Movement.cs
+++ Astral/Assets/_Scripts/AI/AI_Movement.cs
@@ -3,75 +3,71 @@ using System.Collections;
 
 public class AI_Movement : MonoBehaviour {
 
+	public Enemy enemy = new Enemy();
+
 	// Variables For Movement
-	private Vector3 movementSpeed = new Vector2(5.0f, 0.0f);
+	private Vector3 movementSpeed = new Vector2(-5.0f, 0.0f);
 	private float leftLimit = 0.0f;
 	private float rightLimit = 0.0f;
 	private bool firstPass = false; 
 
 	// Variables For Player Detection 
 	public bool playerDetected = false;
-	private Vector3 direction = Vector3.zero;
-	private float previousTransform = 0.0f;
-	private Vector3 rayToCast = Vector3.zero;
-	private float distance = 10.0f;
 
 	// Update is called once per frame
 	void Update () 
 	{
-		/*	START OF MOVEMENT */
-
-		// This sets up the limits - We do this in a first pass just in
-		// case they follow a player. That way they come back to where they started at.
-		// !! Subject to change !!
-		if(!firstPass)
+		if(!playerDetected)
 		{
-			leftLimit = transform.position.x - 10.0f;
-			rightLimit = transform.position.x + 10.0f;
-			firstPass = true;
-		}	
+			/*	START OF MOVEMENT */
 
-		// Controls AI movement
-		transform.position += movementSpeed * Time.deltaTime;
+			// This sets up the limits - We do this in a first pass just in
+			// case they follow a player. That way they come back to where they started at.
+			// !! Subject to change !!
+			if(!firstPass)
+			{
+				leftLimit = transform.position.x - 10.0f;
+				rightLimit = transform.position.x + 10.0f;
+				firstPass = true;
+			}	
 
-		// Changes direction when we reach limits
-		if(transform.position.x >= rightLimit || transform.position.x <= leftLimit)
-		{
-			movementSpeed = -movementSpeed;
-		}
+			// Controls AI movement
+			transform.position += movementSpeed * Time.deltaTime;
 
-		/*	END OF MOVEMENT */
+			// Changes direction when we reach limits
+			if(transform.position.x >= rightLimit || transform.position.x <= leftLimit)
+			{
+				movementSpeed = -movementSpeed;
+				transform.localScale = -transform.localScale;
+			}
 
-		/* 	START OF DETECTION */
-
-		// Use Speed Calculated Before To Find Direction
-		if(movementSpeed.x > 0)
-		{
-			direction = Vector3.right;
-			print("Right");
-		}
-		else
-		{	
-			direction = Vector3.left;
-			print("Left");
-		}
-
-		previousTransform = transform.position.x;
-
-		// RayCast To Hit Our Player
-		//TODO Change this so that we perform two raycasts (eg. one diagonal down and one diagonal up)
-		// and then raycast several rays between them.
-		rayToCast = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
-
-		if(Physics2D.Raycast(rayToCast, direction, distance))
-		{
-			print("Hit something");
-			playerDetected = true;
+			/*	END OF MOVEMENT */
 		}
 		else
 		{
-			playerDetected = false;
+			//TODO Add a wait time until the character is ready to move again
 		}
-
 	}
+
+	/*	START OF DETECTION	*/
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.tag.Equals("Player"))
+		{
+			playerDetected = true;
+			print("Player Detected");
+		}
+	}
+
+	public void OnTriggerExit2D(Collider2D col)
+	{
+		if(col.tag.Equals("Player"))
+		{
+			//playerDetected = false;
+			print("Player Not Detected");
+		}
+	}
+
+	/*	END OF DETECTION	*/
 }
