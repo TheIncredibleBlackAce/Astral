29703eba539b5040d08efa9072190df7697a3e09
TheIncredibleBlackAce <ldyckes@gmail.com>
2016-06-08 18:30:28 -0400

    2nd Trye
    fuck

---
+++ Astral/Assets/_Scripts/Player Scripts/GroundChecker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundChecker : MonoBehaviour {
+
+    private SimpleMovement Movement;
+
+    void Start()
+    {
+        Movement = gameObject.GetComponentInParent<SimpleMovement>();
+    }
+
+    void onTriggerEnter2D(Collision2D col)
+    {
+        Movement.Ground = true;
+    }
+
+    void onTriggerStay2D(Collision2D col)
+    {
+        Movement.Ground = true;
+    }
+
+    void onTriggerExit2D(Collision2D col)
+    {
+        Movement.Ground = false;
+    }
+}
---
+++ Astral/Assets/_Scripts/Player Scripts/SimpleMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovement : MonoBehaviour
+{
+
+    //Public Variables
+    public float MaxSpeed = 5f;
+    public float Friction = .85f;
+    public float JumpStrength = 1000f;
+    public bool Ground = false;
+
+    //Prvate Variables
+    private float Speed;
+    private float CompInputH = 0;
+    private Rigidbody2D RB2D;
+
+    //Input Variables
+    private bool ControlLock = false; //Denies user input when on
+    private float InputH;
+    private bool InputJump;
+
+    //Start
+    private void Start()
+    {
+        RB2D = gameObject.GetComponent<Rigidbody2D>();
+        InputH = Input.GetAxis("Horizontal");
+        InputJump = Input.GetButtonDown("Jump");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ControlLock == false)
+        {
+            InputH = Input.GetAxis("Horizontal");
+            InputJump = Input.GetButtonDown("Jump");
+        }
+        else
+        {
+            InputH = CompInputH;
+        }
+
+        if (Speed < MaxSpeed)
+            Speed += InputH;
+        Speed *= Friction;
+        RB2D.velocity = new Vector2(Speed * 60 * Time.deltaTime, RB2D.velocity.y);
+
+        if (InputJump && Ground)
+        {
+            RB2D.AddForce(Vector2.up * JumpStrength);
+        }
+
+    }
+
+   
+}
